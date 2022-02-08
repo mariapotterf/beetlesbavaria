8128a5e4511151402b8009acf92ad6aee5c525ad
Potterf <ge45lep@mytum.de>
2022-02-08 16:57:36 +0100

    merges data with XY coordinates

--- explore_data.R
+++ explore_data.R
@@ -10,8 +10,8 @@ rm(list=ls())
 library(lubridate)
 library(dplyr)
 library(ggplot2)
-library(rgdal)   # for spatial data
-
+#library(rgdal)   # for spatial data
+library(sf)
 
 # get data
 path = 'C:/Users/ge45lep/Documents/2022_BarkBeetles_Bavaria/rawData/Fwd__Borkenkäferforschung__Datentransfer'
@@ -19,7 +19,7 @@ path = 'C:/Users/ge45lep/Documents/2022_BarkBeetles_Bavaria/rawData/Fwd__Borkenk
 # Load RData table
 load(paste(path, 'BoMo_2015_2021_Rohdaten.RData', sep = "/"))
 
-# Read XY coordinates
+# Read XY coordinates -----------------------------------------------------------
 # need to save the csv fil as a new file, otherwise it did not work
 # or use teh  version 3: now it worked with the original one! 
 xy <- read.table(paste(path, "BoMo_Fallenstandorte_Lage_Laufzeit.csv", sep = '/'), 
@@ -32,15 +32,19 @@ xy <- read.table(paste(path, "BoMo_Fallenstandorte_Lage_Laufzeit.csv", sep = '/'
 # Investigate beetle data: Bavaria
 head(xy)
 
+
+# Convert coordinates to sf object
+xy_sf <- st_as_sf(xy, coords = c('x_coord', 'y_coord' ), crs = 32623 )  # What is a coordinate system??? for forestry in bavaria?
+
+plot(xy_sf["OBJECTID"])
+
+
+# Get dynamics data -------------------------------------------------------------- 
+
 # Change name of the table
+# for dat
 dat <- Daten_B01
 
-
-# Explore the table
-head(dat)
-
-# what beetles species?
-unique(dat$art)
 
 # "Buchdrucker"   "Kupferstecher"
 
@@ -54,17 +58,18 @@ dat$kont_dat <- as.Date(dat$kont_dat)
 
 str(dat)
 
-# Species
-
+# Species:
 # -----------------------------------
 # Buchdrucker  : Ips typographus 
 # Kupferstecher: Pityogenes chalcographus      
+
+
 # Variation within a year?
 # yes, need to split in year, months, dates
 dat <- dat %>% 
-  dplyr::mutate(year = lubridate::year(kont_dat), 
+  dplyr::mutate(year  = lubridate::year(kont_dat), 
                 month = lubridate::month(kont_dat), 
-                day = lubridate::day(kont_dat))
+                day   = lubridate::day(kont_dat))
 
 
 # get basic stats: count by beetles over year, counts by records
@@ -86,7 +91,90 @@ dat %>%
 # 
 summary(dat)
 
+# Join data with XY coordinates by globalid   ---------------------------------------------------
+
+dat <- dat %>% 
+  mutate(globalid =  gsub("\\{|\\}", "", globalid))  
+
+
+# for coordinates table
+
+xy_sf <-
+  xy_sf %>% 
+  mutate(globalid = toupper(globalid))
+
+
+# Merge data by globalid
 # 
+merged_df <- xy_sf %>% 
+  right_join(dat, 'globalid')
+
+
+
+
+
+
+# Plots spatial data ------------------------------------
+
+# 
+windows()
+merged_df %>% 
+  filter(art == 'Buchdrucker') %>% 
+  ggplot() + 
+  geom_sf(aes(color = fangmenge ))  + # , size = 0.5 , size = AREA
+  scale_color_continuous(low = "lightgreen", 
+                         high = "darkgreen",
+                         space = "Lab", 
+                         na.value = "red", guide = "colourbar")#+
+
+
+
+
+# R animate: ------------------------------------------------
+
+# Sums by months and years
+
+buch_sum_ysr <- dat %>%
+  filter(art == 'Buchdrucker') %>% 
+  group_by(globalid, year) %>% 
+  summarize(sum = sum(fangmenge, na.rm = T))
+
+# Merge summarized data with XY 
+buch_df <- xy_sf %>% 
+  right_join(buch_sum_ysr, 'globalid')
+
+
+
+
+# Get data:
+library(gapminder)
+
+# Charge libraries:
+library(ggplot2)
+library(gganimate)
+library(transformr)
+
+
+# need to recheck!! 
+ggplot(buch_df) +   # base map
+  geom_sf(color = 'black', 
+          fill  = 'grey93') + 
+  geom_sf(data = buch_df,
+          aes(color = sum)) + # , size = Age, size = 0.8size by factor itself!
+  viridis::scale_color_viridis(discrete = FALSE, option = "viridis",
+                               na.value = "red") +
+ # annotation_scale(location = "bl", width_hint = 0.4) +
+  theme_bw() +
+  xlab("Longitude") + 
+  ylab("Latitude") +
+  # gganimate specific bits: ------------------
+labs(title = 'Changes in Tree Height over years {current_frame}',
+     color  = "Tree height [m]") +
+  transition_manual(year) +
+  #transition_time(year) +
+  ease_aes('linear')
+
+
 
 
 
