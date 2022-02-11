31f8485c41443e9afb2e15279521aac8aa09e395
Potterf <ge45lep@mytum.de>
2022-02-11 17:48:06 +0100

    get new analyses running:: investigate changes in local vs global factors oevr time

---
+++ analyze_Data.R
@@ -0,0 +1,20 @@
+
+
+# Analyze the data:
+
+#-	Do beetle data correlate with Cornelius data? By hexagons??
+#    -   
+#    - 	Is there less beetles if there is more deciduous trees?
+# 
+#-	Effects of drought: 
+#    -	Is there more beetles from 2018? Eg.g. drought will trigger beetle population
+#    - compare the counts between years    
+# -	Effect of sampling design: do I get the same results given the random selection of two traps?
+
+
+# run analyses on 3km grid:
+
+# First check the all damage together, then distribute it by years
+
+
+  
