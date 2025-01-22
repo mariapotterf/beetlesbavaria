7d2ceea647065a01c9dc97357691a23d21bb0bec
Potterf Mária <potterf@fld.czu.cz>
2025-01-22 09:53:30 +0100

    renamed file name - mypath to myvars

--- myVars.R
+++ myVars.R
@@ -5,7 +5,7 @@
 # ------------------------
 
 # Input paths
-myPath     = "C:/Users/ge45lep/Documents/2022_BarkBeetles_Bavaria"
+#myPath     = "C:/Users/ge45lep/Documents/2022_BarkBeetles_Bavaria"
 inFolder   = "rawData"
 outFolder  = "outSpatial"
 outTable   = "outTable"
